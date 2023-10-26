c8b0e7da5843209941b52dd29431ad2d0a96eed2
jmarzo1 <jorge.marzo@ufl.edu>
2023-10-25 23:23:34 -0400

    Update encoderWilson.py

--- encoderWilson.py
+++ encoderWilson.py
@@ -1,45 +1,46 @@
-
-def encode(inputted_password, encoded_password):
+def encode(inputted_password):
+    encoded_password = []
     for j in inputted_password:
         i = int(j)
         i += 3
         encoded_password.append(i)
     return encoded_password
 
+def decode(encoded_password):
+    decoded_password = []
+    for j in encoded_password:
+        i = int(j)
+        i -= 3
+        decoded_password.append(i)
+    return decoded_password
 
 def main():
-    #main loop to keep the code running until quit
     while True:
-
-        #prints the menu
         print("Menu")
         print("-------------")
         print("1. Encode")
         print("2. Decode")
         print("3. Quit")
 
-        #takes inputted menu option
         menu_selection = int(input("Please enter an option: "))
 
-        #runs statements for menu selection 1
         if menu_selection == 1:
             inputted_password = list(input("Please enter your password to encode: "))
-            encoded_password = []
+            encoded_password = encode(inputted_password)
+            encoded_string = ''.join(map(str, encoded_password))
             print("Your password has been encoded and stored!")
-            #makes the encoded password a string
-            encoded_string = ''.join(map(str, encode(inputted_password, encoded_password)))
 
-        #runs statements for decoder, needs to be FIXME
         if menu_selection == 2:
+            if 'encoded_string' in locals():
+                decoded_password = decode(list(encoded_string))
+                decoded_string = ''.join(map(str, decoded_password))
+                print(f'The encoded password is {encoded_string}')
+                print(f'The decoded password is {decoded_string}')
+            else:
+                print("No password encoded yet.")
 
-            #final print statement
-            print(f'The encoded password is {encoded_string}') #FIXME finish the print statement with decoder
-
-        #exits if 3 is selected
         if menu_selection == 3:
             exit()
 
-
-#runs the program
 if __name__ == '__main__':
     main()
